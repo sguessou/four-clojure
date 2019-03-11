dd06494e62854cccb936d4a268d29c2565e49199
Saad Guessous <guessous_saad@hotmail.com>
2019-03-11 23:37:47 +0200

    added problem #30

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -4,9 +4,30 @@
 ;; Write a function that splits a sentence up into a sorted list of words.
 ;; Capitalization should not affect sort order and punctuation should be ignored.
 ;; e.g. "Have a nice day." --> ["a" "day" "have" "nice"]
-(defn my-sort [s]
+  (defn my-sort [s]
   (sort 
    #(compare (clojure.string/lower-case %1) 
              (clojure.string/lower-case %2)) 
    (clojure.string/split 
     (apply str (remove #((set ",;.") %) s)) #" ")))
+
+;; Day 4
+;; Problem #27
+(= (reverse (seq %)) (seq %))
+
+
+;; #32
+(defn dup [l] 
+  (loop [in l result l]
+    (if (empty? in)
+      (sort  result)
+      (recur (rest in) (conj result (first in))))))
+
+
+;; #30
+(defn p30 [l]
+  (let [res-atom (atom '())] 
+    (doseq [x l]
+      (when (not= (first @res-atom) x)
+        (swap! res-atom (fn [c] (conj c x))))) 
+    (reverse @res-atom)))
